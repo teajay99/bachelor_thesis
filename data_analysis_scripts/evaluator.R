6503d2770d12dbac58ffeabcae3d12a33ababb23
teajay <timojakobs99@gmail.com>
2021-04-19 15:54:11 +0200

    R Script to call Hadron function added and tested

---
+++ data_analysis_scripts/evaluator.R
@@ -0,0 +1,36 @@
+#!/usr/bin/env Rscript
+
+library(hadron)
+
+args = commandArgs(trailingOnly = TRUE)
+
+read_file <- function(path, thermTime) {
+  csvfile = read.csv(path, sep = "\t", header = FALSE)
+  if (ncol(csvfile) == 1) {
+    csvfile = read.csv(path, sep = " ", header = FALSE)
+  }
+  data = csvfile[, c("V2")]
+  data = data[-(1:thermTime)]
+  out = uwerrprimary(data)
+  return(list(val = out$value, err = out$dvalue))
+}
+
+evaluate <- function(dataDirPath, thermTime, N, outFilePath) {
+
+  out = matrix(NA, nrow = N, ncol = 3)
+
+  for (i in 0:(N - 1)) {
+    line = read_file(paste(dataDirPath, "/data-", i, ".csv", sep = ""), thermTime)
+    out[i + 1, 1] = i
+    out[i + 1, 2] = line$val
+    out[i + 1, 3] = line$err
+  }
+  write.table(out, file = outFilePath, sep = "\t", row.names = FALSE, col.names = FALSE)
+}
+
+if (length(args) != 4) {
+  print("4 Args required: data/directory/path #thermalizationIterations #Datasets output/file.csv")
+  q()
+}
+
+evaluate(args[1], as.integer(args[2]), as.integer(args[3]), args[4])
--- data_analysis_scripts/evaluator.r
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env Rscript
-
-
-print("Hello World!")
